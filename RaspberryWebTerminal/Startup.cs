9d629c4cea11b9f131f99caf77f60abef254a0cb
toqsycc <einhorn.sv@gmail.com>
2021-07-06 23:36:43 +0300

    WebUI Index page created

--- RaspberryWebTerminal/Startup.cs
+++ RaspberryWebTerminal/Startup.cs
@@ -27,6 +27,7 @@ namespace RaspberryWebTerminal
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
