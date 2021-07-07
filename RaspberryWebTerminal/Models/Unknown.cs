655701b37297ead34160caa20c2b33c6ba4cf6ab
toqsycc <einhorn.sv@gmail.com>
2021-07-07 18:53:53 +0300

    Добавлена модель Unknown Не содержит в себе ничего полезного - просто напоминание относительно правил нейминга. Также затронута домашняя страница.

---
+++ RaspberryWebTerminal/Models/Unknown.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RaspberryWebTerminal.Models
+{
+    public class Unknown
+    {
+        public readonly string Info = "I am unknown class.";
+        private readonly Guid _classGuid;
+        public string DefaultProperty { get; set; }
+
+        public Unknown()
+        {
+            _classGuid = new Guid();
+            DefaultProperty = "Initialized successfully";
+        }
+
+        public string GenerateReport(int? optionalParameter = 0)
+        {
+            var head = $"Class GUID: {_classGuid};\n";
+            var body = $"Type: Unknown. Info: {Info};\n";
+            var foot = $"Last property value: {DefaultProperty}";
+            var resultString = head + body + foot;
+
+            return resultString;
+        }
+    }
+}
