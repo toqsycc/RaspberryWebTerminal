253f47d0b193330e1f34f087701443d64febd240
toqsycc <einhorn.sv@gmail.com>
2021-07-08 08:17:33 +0300

    Множественные изменения 1. Добавлена поддержка геймпадов на Index Page; 2. Создан контроллер ScriptsController, возвращающий скрипты домашней страницы клиенту; 3. Изменено расположение файлов в директории.

---
+++ RaspberryWebTerminal/Controllers/ScriptsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Logging;
+
+namespace RaspberryWebTerminal.Controllers
+{
+    [Controller]
+    [Route("[controller]")]
+    public class ScriptsController : Controller
+    {
+        private readonly ILogger<ScriptsController> _logger;
+
+        public ScriptsController(ILogger<ScriptsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("gamepad.js")]
+        public ActionResult GetGamepadScript()
+        {
+            string fileName = "/Views/WebUI/gamepad.js";
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + fileName;
+            byte[] fileData = System.IO.File.ReadAllBytes(filePath);
+            string contentType;
+            new FileExtensionContentTypeProvider().TryGetContentType(fileName, out contentType);
+            ContentDisposition content = new ContentDisposition
+            {
+                FileName = fileName,
+                Inline = true
+            };
+            Response.Headers.Add("Content-Disposition", content.ToString());
+            return File(fileData, contentType);
+        }
+    }
+}
